2c48fdc8a99a9df6b30e2aca4aa425bd64ddeda7
Thang Hoang <37979473+hoangthangta@users.noreply.github.com>
2022-10-05 23:36:44 +0700

    Add files via upload
    run faster

--- metrics.py
+++ metrics.py
@@ -8,6 +8,10 @@
 # other metrics (BLEU, BertScore, etc)
 import datasets
 
+meteor = datasets.load_metric('meteor')
+bleu = datasets.load_metric('bleu')
+bertscore = datasets.load_metric('bertscore')
+
 def compute_rouge_batch(predictions, references):
 
     """
@@ -96,13 +100,13 @@ def compute_bertscore_batch(predictions, references):
     result_dict = {}
     prel, re, f1 = [], [], []
 
-    scorer = datasets.load_metric('bertscore')
+    #scorer = datasets.load_metric('bertscore')
     
     for pre, ref in zip(predictions, references):
         print('pre: ', pre)
         print('ref: ', ref)
        
-        output = scorer.compute(predictions=[pre], references=[ref], lang='en')       
+        output = bertscore.compute(predictions=[pre], references=[ref], lang='en')       
         prel.append(output['precision'][0])
         re.append(output['recall'][0])
         f1.append(output['f1'][0])
@@ -127,11 +131,41 @@ def compute_bleu_single(prediction, reference):
     prediction = prediction.split()
     reference = reference.split()
 
-    scorer = datasets.load_metric('bleu')
+    #scorer = datasets.load_metric('bleu')
     
     output = {}
     try:
-        output = scorer.compute(predictions=[prediction], references=[[reference]])
+        output = bleu.compute(predictions=[prediction], references=[[reference]])
+    except:
+        result_dict['bleu'] = 0
+        return result_dict
+
+    #print('output: ', output)
+        
+    result_dict['bleu'] = output['bleu']
+
+    #print('result_dict: ', result_dict)
+    return result_dict
+
+def compute_bleu_batch(predictions, references):
+
+    result_dict = {}
+
+    if (type(predictions) != list or type(references) != list):
+        print('"predictions" or "references" is not a list!')
+        return result_dict
+
+    predictions = [[x.strip('.') for x in pre.split()] for pre in predictions]
+    references = [[x.strip('.') for x in ref.split()] for ref in references]
+
+    #print('predictions: ', predictions)
+    #print('references: ', [references])
+    
+    #scorer = datasets.load_metric('bleu')
+    
+    output = {}
+    try:
+        output = bleu.compute(predictions=predictions, references=[references])
     except:
         result_dict['bleu'] = 0
         return result_dict
@@ -152,15 +186,33 @@ def compute_meteor_single(prediction, reference):
         print('"prediction" or "reference" is not a string!')
         return result_dict
 
-    scorer = datasets.load_metric('meteor')
-    output = scorer.compute(predictions=[prediction], references=[reference])
+    #scorer = datasets.load_metric('meteor')
+    output = meteor.compute(predictions=[prediction], references=[reference])
 
     #print('output: ', output)
         
     result_dict['meteor'] = output['meteor']
 
     #print('result_dict: ', result_dict)
-    return result_dict  
+    return result_dict
+
+
+def compute_meteor_batch(predictions, references):
+
+    result_dict = {}
+    
+    if (type(predictions) != list or type(references) != list):
+        print('"predictions" or "references" is not a list!')
+        return result_dict
+
+    #scorer = datasets.load_metric('meteor')
+    output = meteor.compute(predictions=[predictions], references=[references])
+    #print('output: ', output)
+    result_dict['meteor'] = output['meteor']
+
+    #print('result_dict: ', result_dict)
+    return result_dict
+
 
 def compute_bertscore_single(prediction, reference):
 
@@ -171,8 +223,8 @@ def compute_bertscore_single(prediction, reference):
         return result_dict
     
     # microsoft/deberta-xlarge-mnli (best model), model_type=roberta-large (default)
-    scorer = datasets.load_metric('bertscore')
-    output = scorer.compute(predictions=[prediction], references=[reference], lang='en')
+    #scorer = datasets.load_metric('bertscore')
+    output = bertscore.compute(predictions=[prediction], references=[reference], lang='en')
         
     result_dict['bertscore_precision'] = output['precision']
     result_dict['bertscore_recall'] = output['recall']
@@ -199,4 +251,46 @@ def compute_moverscore_single(prediction, reference):
     score = word_mover_score(reference, prediction, idf_dict_ref, idf_dict_hyp, \
                              stop_words=[], n_gram=1, remove_subwords=True)
     #print('score:' , score)'''
-    return result_dict  
+    return result_dict
+
+#..................
+
+'''from nltk.translate.bleu_score import sentence_bleu
+
+reference = [['the', 'quick', 'brown', 'fox', 'jumped', 'over', 'the', 'lazy', 'dog']]
+candidate = ['the', 'fast', 'brown', 'fox', 'jumped', 'over', 'the', 'sleepy', 'dog']
+
+
+reference1 = ' '.join(x for x in reference[0])
+candidate1 = ' '.join(x for x in candidate)
+
+print('nltk: ', sentence_bleu(reference, candidate))
+'''
+'''from sacrebleu.metrics import BLEU, CHRF, TER
+import sacrebleu
+bleu = BLEU()
+
+
+predictions = ['The', 'film', 'Death', 'on', 'a', 'Factory', 'Farm', 'was', 'shown', 'on', 'HBO', 'and', \
+               'it', 'was', 'written', 'and', 'directed', 'by', 'Tom', 'Simon', 'Geof', 'Bartz', 'is', 'the', \
+               'editor', 'of', 'the', 'film']
+references =  [['Death', 'on', 'a', 'Factory', 'Farm', 'is', 'an', 'HBO', 'Film', 'directed', 'and', 'produced', \
+                 'by', 'Tom', 'Simon', 'Geof', 'Bartz', 'is', 'the', 'editor'],
+                ['Death', 'on', 'a', 'Factory', 'Farm', 'is', 'an', 'HBO', 'film', 'directed,', 'produced,', 'and', \
+                 'edited', 'by', 'Geof', 'Bartz', 'and', 'Tom', 'Simon'],
+                ['Tom', 'Simon', 'directed', 'and', 'produced', 'the', 'film', '‘Death', 'on', 'a', 'Factory', 'Farm’,', \
+                 'which', 'was', 'broadcasted', 'by', 'HBO', 'Geof', 'Bartz', 'on', 'the', 'other', 'hand', 'edited', 'the', \
+                 'film']]
+                
+references1 = [' '.join(x for x in reference) for reference in references]
+predictions1 = [' '.join(x for x in predictions)]
+
+print('references1: ', references1)
+print('predictions1: ', predictions1)
+
+#print('bleu: ', bleu.corpus_score(predictions1, [references1]))
+print('datasets: ', compute_bleu_batch(predictions1, references1))   
+print('sacrebleu: ', sacrebleu.corpus_bleu(predictions1, [references1]))
+
+#from nltk.translate.bleu_score import sentence_bleu
+#print('nltk: ', sentence_bleu(references, predictions))'''
